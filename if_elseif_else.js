bb96177a382f3782f793ad080a6caf568921fb90
Pwdec <76209090+pwdec@users.noreply.github.com>
2021-09-05 02:33:55 +0300

    Create if_elseif_else.js

---
+++ if_elseif_else.js
@@ -0,0 +1,21 @@
+// if - else if - else
+
+/*
+  
+var hourVar = 24
+if(hourVar < 18){
+  console.log("GM");
+}else{
+  console.log("GN");
+}
+
+*/
+  
+var intName = -7
+if(intName < 0){
+  console.log("lesser");
+}else if(intName == 0){
+  console.log("equal");
+}else{
+  console.log("bigger");
+}
