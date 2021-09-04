c710c5568737e852cb563ee54781fb5dc0c2189a
Pwdec <76209090+pwdec@users.noreply.github.com>
2021-09-05 01:51:34 +0300

    Create Conditionals.js

---
+++ Conditionals.js
@@ -0,0 +1,11 @@
+== -> equal to
+=== -> equal value and equal type
+!= -> not equal
+>  -> greater than
+<  -> less than
+>= -> greater than or equal to
+<= -> less than or equal to
+
+&& -> and
+|| -> or
+!  -> not
