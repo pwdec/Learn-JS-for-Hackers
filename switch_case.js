f1a4c5940cf23c58570d46c0aad0e3034de76b9b
Pwdec <76209090+pwdec@users.noreply.github.com>
2021-09-05 02:47:31 +0300

    Create switch_case.js

---
+++ switch_case.js
@@ -0,0 +1,18 @@
+var num = 6;
+switch(num){
+  case 1:
+    console.log("one");
+  break;
+  case 2:
+    console.log("two");
+  break;
+  case 3:
+    console.log("three");
+  break;
+  
+  default:
+    console.log("1-3 pls");
+  break;
+  
+  
+}
