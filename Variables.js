ee206544f1a8add862fb37fcf8106164adfd39f2
Pwdec <76209090+pwdec@users.noreply.github.com>
2021-09-04 18:02:04 +0300

    Create Variables.js

---
+++ Variables.js
@@ -0,0 +1,14 @@
+// var -> Variablesm
+// 5 --> Integer, "6" --> String, "pwdec" --> String
+// True Usage;
+// var variable = 31;
+// var variable = 31;
+// var variable_Name = 31;
+
+
+
+var name = "Pwdec";
+var age = 21;
+
+
+alert("My name is " + name + " " + "I'm " + age);
