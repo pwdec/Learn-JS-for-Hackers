20fb1505313065a29fbdefb9a26355d4926f9f19
Pwdec <76209090+pwdec@users.noreply.github.com>
2021-09-04 18:03:27 +0300

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,26 @@
+// 1 = True, 0 = False
+// var testBool = true; & var testBool =false;
+// [1,2,3,4] --> 0.index=1 , 1.index=2 ..
+
+//Usage
+var myArray = [false,31,"pwdec"];
+console.log(myArray);
+console.log(myArray[2]);
+
+//Add
+myArray[3] = "Array";
+console.log(myArray);
+
+//Replace
+myArray[1] = "Replaced";
+console.log(myArray);
+
+// With +
+console.log("Hi" + " " + myArray[1]);
+
+
+
+console.log(myArray.length)
+
+myArray.push("pushworks")
+console.log(myArray)
