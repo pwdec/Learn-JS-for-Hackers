dc307ef5f1868657873631051ef8ed871354eac5
Pwdec <76209090+pwdec@users.noreply.github.com>
2021-09-05 13:27:28 +0300

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,49 @@
+/* 
+function myFunction(msg){
+  alert(msg);
+}
+var message = "one message";
+myFunction(message);
+myFunction();
+
+
+  
+
+function mySum(num1,num2){
+alert(num1 + num2);
+}
+mySum(3,1);
+
+// Return
+
+function sum2(num3,num4){
+  return num3+num4;
+  
+}
+
+var result = 6+5;
+console.log(result);
+
+*/
+  
+// Example
+
+
+
+function sum3(num5,num6){
+  if((num5+num6)== 31){
+    return true;
+  }else{
+    return false;
+  }
+  
+}
+
+if(sum3(30,1)){
+  alert("Yep");
+  }else{
+  alert("Nop");
+}
+
+
+
